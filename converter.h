255d80ebe8d598722c3066081b9e4424279b22dc
Yuri Tikhonov <tikhonov.yuri@gmail.com>
2022-11-12 17:10:01 +0300

    12-11-22

--- converter.h
+++ converter.h
@@ -1,10 +1,66 @@
+#pragma once
 #include <vector>
 #include <string>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdexcept>
+#include <nlohmann/json.hpp>
+//template to fill config
+/*  std::ofstream file("..//config.json");
+  nlohmann::json temp = {
+          {"config", {
+              "name", "SearchEngine",
+                      "version", "0.1",
+                      "max_responses", 5
+          }},
+          { "files", {
+          "../resources/file001.txt",
+          "../resources/file002.txt",
+          "../resources/file003.txt",
+           "../resources/file004.txt"
+
+          }}
+  };
+  file << temp;
+  file.close();*/
+//
+
 
 class ConverterJSON {
 public:
     ConverterJSON() = default;
-    /*
+
+    static std::vector<std::string>Starting()
+     {
+        std::ifstream file("config.json");
+         if(!file.is_open())
+         {
+             throw std::range_error("config file is missing");
+
+         }
+        nlohmann::json startControl;
+        std::vector<std::string>startId;
+
+            file >> startControl;
+
+        auto it = startControl.begin();
+        std::string  appConfig = it.key();
+        std::string appName = startControl.at("config").at(1);
+         if(appName == "")
+         {
+             throw std::exception();
+         }
+        std::string versionNumber = startControl.at("config").at(3);
+        startId.push_back(appConfig);
+        startId.push_back(appName);
+        startId.push_back(versionNumber);
+
+        file.close();
+        return startId;
+     }
+
+        /*
  * Метод получения содержимого файлов
  * @return Возвращает список с содержимым файлов перечисленных
  * в config.json
--- main.cpp
+++ main.cpp
@@ -1,51 +1,19 @@
 #include "converter.h"
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <nlohmann/json.hpp>
 
-struct Start
-{
-    std::string checkConfig;
-    std::string checkVersion;
-};
 
 int main() {
-    Start start;
-    //
-  /*  std::ofstream file("..//config.json");
-    nlohmann::json temp = {
-            {"config", {
-                "name", "SearchEngine",
-                        "version", "0.1",
-                        "max_responses", 5
-            }},
-            { "files", {
-            "../resources/file001.txt",
-            "../resources/file002.txt",
-            "../resources/file003.txt",
-             "../resources/file004.txt"
+    ConverterJSON*  convert = new ConverterJSON;
+    std::vector<std::string>startCheck;
+    try {
+        startCheck = convert->Starting();
+    }
+    catch (std::exception& x) {
+            std::cout << x.what() <<  std::endl;
+            return 0;
+        }
 
-            }}
-    };
-
-
-
-
-    file << temp;
-    file.close();*/
-    //
-    std::ifstream file("..//config.json");
-    nlohmann::json startControl;
-    file >> startControl;
-
-    std::cout << startControl.at("config").at(1) << std::endl;
-
-    std::cout << startControl.at("config").at(3) << std::endl;
-
-
-    std::cout << startControl.at("config").at(5) << std::endl;
-
-std::cout << "Good luck with your project!";
+    std::cout  << startCheck[1] << " " << startCheck[2]  <<  " is started" << std::endl;
+    std::cout << "Good luck with your project!";
+    delete convert;
     return 0;
 }
